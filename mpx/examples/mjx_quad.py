c63ef6984e7ad03d6420351a792950a624431b86
undefined-panda <jad.taim2003@gmail.com>
2025-12-08 14:36:45 +0100

    code to create custom dataset, prep for running multiple simulations

--- mpx/examples/mjx_quad.py
+++ mpx/examples/mjx_quad.py
@@ -1,4 +1,3 @@
-
 import jax.numpy as jnp
 import jax
 import mujoco
@@ -16,6 +15,9 @@ import mpx.utils.mpc_wrapper as mpc_wrapper
 import mpx.config.config_aliengo as config
 
 from timeit import default_timer as timer
+
+from pathlib import Path # ADDED BY ME
+
 # Set GPU device for JAX
 # gpu_device = jax.devices('gpu')[0]
 # jax.default_device(gpu_device)
@@ -33,82 +35,180 @@ state_observables_names = tuple(QuadrupedEnv.ALL_OBS)  # return all available st
  
 # Initialize simulation environment
 sim_frequency = 200.0
-env = QuadrupedEnv(robot=robot_name,
-                   scene=scene_name,
-                   sim_dt = 1/sim_frequency,  # Simulation time step [s]
-                   ref_base_lin_vel=0.0, # Constant magnitude of reference base linear velocity [m/s]
-                   ground_friction_coeff=0.7,  # pass a float for a fixed value
-                   base_vel_command_type="human",  # "forward", "random", "forward+rotate", "human"
-                   state_obs_names=state_observables_names,  # Desired quantities in the 'state'
-                   )
-obs = env.reset(random=False)
-# Define the MPC wrapper
-mpc = mpc_wrapper.MPCControllerWrapper(config)
-env.mjData.qpos = jnp.concatenate([config.p0, config.quat0,config.q0])
-env.render()
-ids = []
-# for i in range(8):
-#      ids.append(render_vector(env.viewer,
-#               np.zeros(3),
-#               np.zeros(3),
-#               0.1,
-#               np.array([1, 0, 0, 1])))
-counter = 0
-# Main simulation loop
-tau = jnp.zeros(config.n_joints)
-tau_old = jnp.zeros(config.n_joints)
-delay = int(0.007*sim_frequency)
+delay = 0 #int(0.007*sim_frequency)
 print('Delay: ',delay)
 
-q = config.q0.copy()
-dq = jnp.zeros(config.n_joints)
-mpc_time = 0
-mpc.robot_height = config.robot_height
-mpc.reset(env.mjData.qpos.copy(),env.mjData.qvel.copy())
-while env.viewer.is_running():
- 
-    qpos = env.mjData.qpos.copy()
-    qvel = env.mjData.qvel.copy()
-    if (counter % (sim_frequency / mpc_frequency) == 0 or counter == 0):
+# BEGIN ADDED BY ME: dataset, log_values, save_dataset, for-loop
+dataset_path = Path.cwd() / "custom_datasets"
+dataset_path.mkdir(exist_ok=True)
+
+# add values to dataset
+def log_values(dataset, sim_num, t, qpos, qvel, qacc, tau_total, contact, contact_forces, q):
+    """
+    Docstring for log_values
     
- 
-        ref_base_lin_vel = env._ref_base_lin_vel_H
-        ref_base_ang_vel =  np.array([0., 0., env._ref_base_ang_yaw_dot])
- 
-        
-        input = np.array([ref_base_lin_vel[0],ref_base_lin_vel[1],ref_base_lin_vel[2],
-                           ref_base_ang_vel[0],ref_base_ang_vel[1],ref_base_ang_vel[2],
-                           config.robot_height])
-        
-        contact_temp, _ = env.feet_contact_state()
-        
-        contact = np.array([contact_temp[robot_feet_geom_names[leg]] for leg in ['FL','FR','RL','RR']])
+    :param dataset: dictionary to store values
+    :param qpos: linear, angular and joint position
+    :param qvel: linear, angular and joint velocity
+    :param qacc: linear, angular and joint accelaration
+    :param tau_total: tau + tau_fb
+    :param contact: boolean array with elem for each contact (here: 4)
+    :param contact_forces: 2d-array with array for each contact (here: 4)
+    :param q: desired joint position/angle
+    """
 
-        if counter != 0:
-            for i in range(delay):
-                qpos = env.mjData.qpos.copy()
-                qvel = env.mjData.qvel.copy()
-                # tau_fb = K@(x-np.concatenate([qpos,qvel]))
+    dataset["time"][sim_num].append(t)
 
-                tau_fb = 10*(q-qpos[7:7+config.n_joints]) -2*(qvel[6:6+config.n_joints])
-                state, reward, is_terminated, is_truncated, info = env.step(action=tau + tau_fb)
-                counter += 1
-        start = timer()
-        tau, q, dq = mpc.run(qpos,qvel,input,contact)   
-        stop = timer()
-        print("Time taken for MPC: ", stop-start)   
+    # --- Base ---
+    dataset["base_pos"][sim_num].append(qpos[0:3].copy())
+    dataset["base_orient"][sim_num].append(qpos[3:7].copy())
+    dataset["base_vel"][sim_num].append(qvel[0:3].copy())
+    dataset["base_acc"][sim_num].append(qacc[0:3].copy())
 
-        stop = timer()
-        # for i in range(4):
-        #     render_sphere(env.viewer,
-        #                   collision_point[3*i:3*i+3],
-        #                   0.2,
-        #                   np.array([1, 0, 0, 0.5]),
-        #                   ids[i])
+    # --- Joint ---
+    dataset["joint_pos"][sim_num].append(qpos[7:].copy())
+    dataset["joint_vel"][sim_num].append(qvel[6:].copy())
+    dataset["joint_acc"][sim_num].append(qacc[6:].copy())
+    dataset["joint_torque"][sim_num].append(tau_total.copy())
+    
+    # --- Contact ---
+    dataset["contact_state"][sim_num].append(contact.copy())
+    dataset["contact_forces"][sim_num].append(contact_forces.copy())
+    dataset["contact_pos_des"][sim_num].append(q.copy())
 
-    tau_fb = 10*(q-qpos[7:7+config.n_joints])-2*(qvel[6:6+config.n_joints])
-    state, reward, is_terminated, is_truncated, info = env.step(action= tau + tau_fb)
+def save_dataset(dataset):
+    # convert data to numpy array
+    for i in dataset:
+        dataset[i] = np.stack(dataset[i], axis=0)
 
-    # time.sleep(0.1)
-    counter += 1
+    next_run = 0
+    current_run = 1
+    for file in Path(dataset_path).glob("*"):
+        current_run = int(file.stem.split("run", 1)[1])
+        if current_run > next_run:
+            next_run = current_run + 1
+
+    np.savez(f"{dataset_path}/quad_dataset_run{next_run}.npz", **dataset)
+    print("Data saved.")
+
+# store values in lists, convert them later to numpy arrays
+custom_dataset = {"time":[],
+                  "base_pos":[],
+                  "base_orient":[],
+                  "base_vel":[],
+                  "base_acc":[],
+                  "joint_pos":[],
+                  "joint_vel":[],
+                  "joint_acc":[],
+                  "joint_torque":[],
+                  "contact_state":[],
+                  "contact_forces":[],
+                  "contact_pos_des":[]
+                  }
+
+num_simulations = 1
+custom_dataset = {k : [[] for _ in range(num_simulations)] for k in custom_dataset} # add one list per simulation: "base_pos" : [[], [], ...]
+log_and_save = False
+
+for sim_num in range(num_simulations):
+
+    # ADDED BY ME: start new simulation when window is closed
+    env = QuadrupedEnv(robot=robot_name,
+                       scene=scene_name,
+                       sim_dt = 1/sim_frequency,  # Simulation time step [s]
+                       ref_base_lin_vel=0.0, # Constant magnitude of reference base linear velocity [m/s]
+                       ground_friction_coeff=0.7,  # pass a float for a fixed value
+                       base_vel_command_type="human",  # "forward", "random", "forward+rotate", "human"
+                       state_obs_names=state_observables_names,  # Desired quantities in the 'state'
+                       )
+    obs = env.reset(random=False)
+
+    # Define the MPC wrapper
+    mpc = mpc_wrapper.MPCControllerWrapper(config)
+    env.mjData.qpos = jnp.concatenate([config.p0, config.quat0,config.q0])
     env.render()
+    counter = 0
+
+    # Main simulation loop
+    tau = jnp.zeros(config.n_joints)
+    tau_old = jnp.zeros(config.n_joints)
+
+    q = config.q0.copy()
+    dq = jnp.zeros(config.n_joints)
+    mpc_time = 0
+    mpc.robot_height = config.robot_height
+    mpc.reset(env.mjData.qpos.copy(),env.mjData.qvel.copy())
+
+    while env.viewer.is_running():
+    
+        qpos = env.mjData.qpos.copy()
+        qvel = env.mjData.qvel.copy()
+        qacc = env.mjData.qacc.copy() # ADDED BY ME: get base and joint accelaration
+        if (counter % (sim_frequency / mpc_frequency) == 0 or counter == 0):
+            
+            ref_base_lin_vel = env._ref_base_lin_vel_H
+            ref_base_ang_vel =  np.array([0., 0., env._ref_base_ang_yaw_dot])
+
+            # ADDED BY ME: sample values to make the robot move on its own
+            # sample a velocity in the direction the robot is facing
+            if counter != 0:
+                vx = np.random.uniform(0, 0.5) 
+                ref_base_lin_vel = np.array([vx, 0, 0])
+            
+                # sample a angular rotation to turn the robot to left/right
+                if counter % ((sim_frequency / mpc_frequency) * 10) == 0:
+                    az = np.random.uniform(-1, 1) 
+                    ref_base_ang_vel = np.array([0, 0, az])
+
+            print(f"base vel: {ref_base_lin_vel} \t base ang: {ref_base_ang_vel}")
+
+            input = np.array([ref_base_lin_vel[0],ref_base_lin_vel[1],ref_base_lin_vel[2],
+                            ref_base_ang_vel[0],ref_base_ang_vel[1],ref_base_ang_vel[2],
+                            config.robot_height])
+            
+            contact_temp, _, contact_forces_temp = env.feet_contact_state(ground_reaction_forces=True) # ADDED BY ME: set parameter to get contact forces
+            
+            contact = np.array([contact_temp[robot_feet_geom_names[leg]] for leg in ['FL','FR','RL','RR']])
+            contact_forces = np.array([contact_forces_temp[robot_feet_geom_names[leg]] for leg in ['FL','FR','RL','RR']]) # ADDED BY ME: get values for contact forces
+
+            if counter != 0:
+                for i in range(delay):
+                    qpos = env.mjData.qpos.copy()
+                    qvel = env.mjData.qvel.copy()
+                    qacc = env.mjData.qacc.copy() # ADDED BY ME: get base and joint accelaration
+                    # tau_fb = K@(x-np.concatenate([qpos,qvel]))
+
+                    tau_fb = 10*(q-qpos[7:7+config.n_joints]) -2*(qvel[6:6+config.n_joints])
+                    state, reward, is_terminated, is_truncated, info = env.step(action=tau + tau_fb)
+
+                    t = env.mjData.time 
+                    log_values(custom_dataset, sim_num, t, qpos, qvel, qacc, tau+tau_fb, contact, contact_forces, q) # ADDED BY ME: add values to dataset
+
+                    counter += 1
+
+            start = timer()
+            tau, q, dq = mpc.run(qpos,qvel,input,contact)   
+            stop = timer()
+            #print("Time taken for MPC: ", stop-start)
+
+            stop = timer()
+            # for i in range(4):
+            #     render_sphere(env.viewer,
+            #                   collision_point[3*i:3*i+3],
+            #                   0.2,
+            #                   np.array([1, 0, 0, 0.5]),
+            #                   ids[i])
+
+        tau_fb = 10*(q-qpos[7:7+config.n_joints])-2*(qvel[6:6+config.n_joints])
+        state, reward, is_terminated, is_truncated, info = env.step(action= tau + tau_fb)
+
+        t = env.mjData.time
+        log_values(custom_dataset, sim_num, t, qpos, qvel, qacc, tau+tau_fb, contact, contact_forces, q) # ADDED BY ME: add values to dataset
+
+        # time.sleep(0.1)
+        counter += 1
+        env.render()
+
+    print(f"\n----- Simulation {i+1} finished -----\n")
+
+if log_and_save: save_dataset(custom_dataset) # ADDED BY ME: save dataset of simulations
